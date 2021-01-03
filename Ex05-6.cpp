77535822cfc03f507a834cb1d5950abefd44a87a
Milind Singh <milind345@gmail.com>
2021-01-02 20:07:15 -0500

    Adds more Ex05_6 methods

--- Ex05-6.cpp
+++ Ex05-6.cpp
@@ -50,11 +50,37 @@ public:
 		}
 		return count;
 	}
+
+	int editsToNumberXOR2(uint32_t first, uint32_t second) {
+		int count = 0;
+		// Same method as above , shortened loop
+		for (uint32_t i = first ^ second; i != 0; i >>= 1) {
+			count += (i & 1);
+		}
+		return count;
+	}
+
+	/*
+	 Saves even more iterations , just checks how many bits are 1 without traversing the whole number
+		if number is 10100	it will do
+		1.     10100 & 10011 = 10000
+		2.     10000 & 01111 = 00000
+		Count = 2
+	*/
+	int editsToNumberXOR3(uint32_t first, uint32_t second) {
+		int count = 0;
+		for (uint32_t i = first ^ second; i != 0; i = i & (i - 1)) {
+			count++;
+		}
+		return count;
+	}
 };
 
 int main() {
 	Ex_05_6 solution;
-	cout << solution.editsToNumber(29, 15) << endl;
-	cout << solution.editsToNumberXOR(29, 15) << endl;
+	cout << solution.editsToNumber(1, 15) << endl;
+	cout << solution.editsToNumberXOR(1, 15) << endl;
+	cout << solution.editsToNumberXOR2(1, 15) << endl;
+	cout << solution.editsToNumberXOR3(1, 15) << endl;
 	return 0;
 }
