3cf497ca8524ae2558a03ee0ba30b9c82a8e8efa
Milind Singh <milind345@gmail.com>
2021-01-02 19:56:47 -0500

    Ex05_06 XOR Method

--- Ex05-6.cpp
+++ Ex05-6.cpp
@@ -33,10 +33,28 @@ public:
 		}
 		return count;
 	}
+
+	int editsToNumberXOR(uint32_t first, uint32_t second) {
+		uint64_t acc = first ^ second;
+		int count = 0;
+		while (acc != 0)
+		{
+			// Extract number from XORd result , this will be 0 where bits were different
+			bool bit = acc & 1;
+			// Count bits which are 1
+			if (bit) {
+				count++;
+			}
+			// Shift right to keep moving
+			acc >>= 1;
+		}
+		return count;
+	}
 };
 
 int main() {
 	Ex_05_6 solution;
-	cout << solution.editsToNumber(29, 29);
+	cout << solution.editsToNumber(29, 15) << endl;
+	cout << solution.editsToNumberXOR(29, 15) << endl;
 	return 0;
 }
