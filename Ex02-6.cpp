b6295f7de5b42e291d3c9f90f24142458588a885
Milind Singh <milind345@gmail.com>
2021-01-13 23:44:07 -0500

    Ex_02_6 LinkedList Palindrome

---
+++ Ex02-6.cpp
@@ -0,0 +1,65 @@
+/*
+	2.6 Palindrome: Implement a function to check if a linked list is a palindrome
+
+*/
+
+#include <iostream>
+#include <stack>
+#include <string>
+#include "LinkedList.h"
+
+using std::cout;
+using std::endl;
+using std::stack;
+using std::string;
+
+class EX_02_06 {
+public:
+	EX_02_06() {
+	}
+
+	template<typename T>
+	static bool isPalindromeList(LinkedList<T>& list) {
+		// Get number of elements
+		size_t count = list.getSize() + 1;
+		size_t half = count / 2;
+
+		// Stack to track half list elements
+		stack<T> track;
+		LinkedList<T>::Iterator runPtr = list.begin();
+		// Push into stack
+		while (half-- > 0) {
+			track.push(runPtr->val);
+			runPtr++;
+		}
+
+		// Adjust for odd elements
+		if (count % 2 == 1) { runPtr++; }
+
+		// Now we are at half point , pop from stack and keep reading list , if any value is a mismatch then break
+		while (runPtr != list.end()) {
+			T val = track.top();
+			if (val != runPtr->val) { return false; }
+			runPtr++;
+			track.pop();
+		}
+
+		return true;
+	}
+};
+
+//int main() {
+//	LinkedList<string> pal;
+//	pal.append("Hello");
+//	pal.append("HI");
+//	pal.append("Bye");
+//	pal.append("Bye");
+//	pal.append("HI");
+//	pal.append("Hello");
+//
+//	pal.printList();
+//
+//	cout << (EX_02_06::isPalindromeList<string>(pal) ? "True" : "False");
+//
+//	return 0;
+//}
