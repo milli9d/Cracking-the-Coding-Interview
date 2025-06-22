39f47c73e316f027bae58167640504e8759f22b5
Milind Singh <milind345@gmail.com>
2025-06-21 23:47:02 -0700

    new ring buffer

--- 2025/lib/ring_buffer/include/ring_buffer.hpp
+++ 2025/lib/ring_buffer/include/ring_buffer.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <mutex>
 #include <memory>
 
 namespace ctci {
@@ -8,91 +9,170 @@ template<typename T, size_t S>
 class ring_buf
 {
   private:
-    size_t _sz{ 0u };
+    std::shared_ptr<T[]> _data{};
+    std::mutex _mtx{};
+
     size_t _cap{ S };
+    size_t _sz{ 0u };
 
-    size_t _tail{ 0u };
     size_t _head{ 0u };
-
-    std::shared_ptr<T[]> _data{ nullptr };
+    size_t _tail{ 0u };
 
   public:
     /**
-     * @brief Constructor
+     * @brief overload the << operator
+     * @param os
+     * @param obj
+     * @return
      */
-    ring_buf(void)
-    {
-        /* allocate the buffer */
-        _data = std::shared_ptr<T[]>(new T[_cap]);
-        if (!_data) {
-            throw std::runtime_error("Out of memory!");
-        }
-    };
-
     friend std::ostream& operator<<(std::ostream& os, ring_buf<T, S>& obj)
     {
+        std::lock_guard<std::mutex> _lock(obj._mtx);
         os << "Size = " << obj.size() << std::endl;
-
         for (size_t i = 0u; i < obj._cap; i++) {
             os << obj._data[i];
-
             if (obj._head == i) {
                 os << " <- HEAD";
             }
-
             if (obj._tail == i) {
                 os << " <- TAIL";
             }
-
-            os << std::endl;
+            os << " |";
         }
-
+        os << std::endl;
         return os;
     }
 
+    ring_buf()
+    {
+        _data = std::make_shared<T[]>(S);
+    }
+
+    ~ring_buf() {}
+
+    /**
+     * @brief push a single value into the buffer
+     * @param val
+     */
+    void push(const T& val)
+    {
+        std::lock_guard<std::mutex> _lock(_mtx);
+        if (_sz + 1u > _cap) {
+            printf("ERROR; Out of memory\n");
+            return;
+        }
+
+        _data[_head] = val;
+        _head = (_head + 1u) % _cap;
+        _sz++;
+    }
+
+    /**
+     * @brief push an array of data into the buffer
+     * @param data
+     * @param data_sz
+     */
+    void push(T* data, size_t data_sz)
+    {
+        std::lock_guard<std::mutex> _lock(_mtx);
+        if (_sz + data_sz > _cap) {
+            printf("ERROR; Out of memory\n");
+            return;
+        }
+
+        /* if we are going to wrap, handle here */
+        size_t size_till_end = _cap - _head;
+        if (data_sz > size_till_end) {
+            memcpy(_data.get() + _head, data, size_till_end * sizeof(T));
+            _head = (_head + size_till_end) % _cap;
+            _sz += size_till_end;
+            data = data + size_till_end;
+            data_sz = data_sz - size_till_end;
+        }
+
+        /* write rest of data; OR all data */
+        memcpy(_data.get() + _head, data, data_sz * sizeof(T));
+        _head = (_head + data_sz) % _cap;
+        _sz += data_sz;
+    }
+
+    /**
+     * @brief pop an element
+     */
+    void pop()
+    {
+        std::lock_guard<std::mutex> _lock(_mtx);
+        if (!_sz) {
+            printf("ERROR; no more elements\n");
+            return;
+        }
+
+        _tail = (_tail + 1u) % _cap;
+        _sz--;
+    }
+
+    /**
+     * @brief pop multiple elements
+     * @param sz
+     */
+    void pop(size_t sz)
+    {
+        std::lock_guard<std::mutex> _lock(_mtx);
+        if (sz > _sz) {
+            printf("ERROR: not enough elements\n");
+            return;
+        }
+
+        _tail = (_tail + sz) % _cap;
+        _sz -= sz;
+    }
+
+    /**
+     * @brief read the specified amount of bytes
+     * @param data
+     * @param data_sz
+     */
+    void read(T* data, size_t data_sz)
+    {
+        std::lock_guard<std::mutex> _lock(_mtx);
+        /* sanity check */
+        if (data_sz > _sz) {
+            printf("ERROR: not enough data to read\n");
+            return;
+        }
+
+        /* read till end of buffer */
+        size_t size_till_end = _cap - _tail;
+        if (data_sz > size_till_end) {
+            memcpy(data, _data.get() + _tail, size_till_end * sizeof(T));
+            _tail = (_tail + size_till_end) % _cap;
+            data_sz -= size_till_end;
+            data += size_till_end;
+            _sz -= size_till_end;
+        }
+
+        /* read the rest of data */
+        memcpy(data, _data.get() + _tail, data_sz * sizeof(T));
+        _tail = (_tail + data_sz) % _cap;
+        _sz -= data_sz;
+    }
+
+    /**
+     * @brief size of the buffer in use
+     * @return
+     */
     size_t size()
     {
         return _sz;
     }
 
     /**
-     * @brief
+     * @brief capacity of the buffer
+     * @return
      */
-    int32_t push(const T& val)
+    size_t cap()
     {
-        /* sanity check  */
-        if (_sz >= _cap) {
-            printf("No more space!\n");
-            return -ENOMEM;
-        }
-
-        /* calculate next index */
-        _data[_tail] = val;
-        _tail = (_tail + 1u) % _cap;
-        _sz++;
-
-        return 0;
-    }
-
-    int32_t pop(T& val)
-    {
-        /* sanity check */
-        if (_sz <= 0) {
-            printf("Empty buffer.\n");
-            return -ENODATA;
-        }
-
-        val = _data[_head++];
-        _sz--;
-        return 0;
-    }
-
-    int32_t clear()
-    {
-        _sz = 0u;
-        _tail = 0u;
-        _head = 0u;
-        return 0;
+        return _cap;
     }
 };
 
--- 2025/lib/ring_buffer/ring_buffer_tb.cpp
+++ 2025/lib/ring_buffer/ring_buffer_tb.cpp
@@ -5,30 +5,35 @@
 
 int main()
 {
-    printf("Ring Buffer Testbench\n");
-    ctci::ring_buf<int, 5> rb{};
-    srand(time(0));
+    ctci::ring_buf<uint32_t, 16u> _buf;
 
-    for (size_t i = 0u; i < 5u; i++) {
-        rb.push((int)i);
+    for (size_t i = 0u; i < _buf.cap(); i++) {
+        _buf.push(i);
+    }
+    std::cout << _buf;
+
+    std::array<uint32_t, 32u> _temp_data;
+    for (size_t i = 0u; i < _temp_data.size(); i++) {
+        _temp_data[i] = 100 + i;
     }
 
-    std::cout << rb << std::endl;
 
-    for (size_t i = 0u; i < 3u; i++) {
-        int out = 0;
-        rb.pop(out);
+    std::array<uint32_t, 12u> _read_data;
+    _buf.read(_read_data.data(), _read_data.size());
+    for (auto i : _read_data) {
+        printf("%d\n", i);
     }
+    std::cout << _buf;
 
-    std::cout << rb << std::endl;
-
-    rb.clear();
-
-    for (size_t i = 0u; i < 5u; i++) {
-        rb.push((int)i * 50);
+    _buf.push(_temp_data.data(), 12u);
+    std::cout << _buf;
+    
+    _buf.push(_temp_data.data() + 12u, 6u);
+    _buf.read(_read_data.data(), _read_data.size());
+    for (auto i : _read_data) {
+        printf("%d\n", i);
     }
-
-    std::cout << rb << std::endl;
+    std::cout << _buf;
 
     return 0;
 }
